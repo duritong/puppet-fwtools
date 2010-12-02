6935e556ad6fc337012527ae4f31bb8f4489ad9f
mh <mh@immerda.ch>
2010-12-02 23:47:03 +0100

    plural everywhere

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -6,8 +6,8 @@ class fwtools::linux {
   include ::shorewall
 
   fwtools::linux::tool{
-    [ 'fwtool_show_conns_per_port',
-      'fwtool_block_ips',
+    [ 'fwtools_show_conns_per_port',
+      'fwtools_block_ips',
       'fwtools_defend_host',
       'fwtools_mirror_blocks' ]:
   }
