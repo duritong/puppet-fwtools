82b2f89d5a446d5a5780480640a546cba2cc6ed8
mh <mh@immerda.ch>
2010-12-05 19:22:01 +0100

    finish correct naming

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -5,7 +5,7 @@ class fwtools::linux inherits fwtools::base {
 
   include ::shorewall
 
-  fwtools::linux{
+  fwtools::tool{
     [ 'fwtools_show_conns_per_port',
       'fwtools_block_ips',
       'fwtools_defend_host' ]:
