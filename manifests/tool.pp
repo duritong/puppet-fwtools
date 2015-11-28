dccfbeaeacacd9fa38ca209d441af98b53ab85a0
mh <mh@immerda.ch>
2015-11-28 17:41:24 +0100

    linting for future parser

--- manifests/tool.pp
+++ manifests/tool.pp
@@ -1,8 +1,11 @@
+# deploy a tool
 define fwtools::tool(){
   file{
     "/usr/local/sbin/${name}":
       source => [ "puppet:///modules/fwtools/${::kernel}/${name}",
                   "puppet:///modules/fwtools/${name}" ],
-      owner => root, group => 0, mode => 0700;
+      owner  => root,
+      group  => 0,
+      mode   => '0700';
   }
 }
